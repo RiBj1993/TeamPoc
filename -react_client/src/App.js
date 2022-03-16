4223f5a64d2a11d5b11f25a603d296032bd5ff8f
Rihab BEJI <rihabbeji@yahoo.fr>
2022-03-16 15:33:13 +0300

    update

---
+++ -react_client/src/App.js
@@ -0,0 +1,221 @@
+ import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './App.css';
+import StockChart from "./components/stockChart";
+import React, { useState,useEffect, useRef, useCallback, useMemo } from "react";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { MdOutgoingMail } from "react-icons/md";
+import { AiFillLinkedin } from "react-icons/ai";
+import { BsInstagram } from "react-icons/bs";
+import "./Contact.css";
+import {variables} from './Variables.js';
+function App() {
+  const [display, setDiplay] = useState(false);
+
+  const [date, setDate] = useState([
+    new Date(2005, 6, 1),
+    new Date(2050, 6, 10),
+  ]);
+
+  const ob={ob:{
+    stockFullName: "SW Limited.",
+    stockShortName: "ASX:SFW",
+    price: {
+      current: 2.32,
+      open: 2.23,
+      low: 2.215,
+      high: 2.325,
+      cap: 93765011,
+      ratio: 20.1,
+      dividend: 1.67,
+    },
+    chartData: {
+      labels:[4,5],
+      data: [5,4],
+    },
+  }}
+  const [data, setData] = useState(ob);
+  useEffect(() => {
+    get( startDate,endDate);}, []);
+
+async function get( startDate,endDate){
+    await getEvents(startDate,endDate);
+}
+  async function getEvents(startDate,endDate){
+       console.log(startDate,endDate);
+
+   if(date[1]!= null){
+    fetch(variables.API_URL+'getAllAggBetweenDates', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+         "date1": new Date(''+startDate.toISOString() ),
+  "date2": new Date(''+endDate.toISOString())
+        }
+          
+          )
+          
+      })
+      .then((res) => res.json())
+      .then((data) => {
+ 
+        const info = {ob:{
+          stockFullName: "RSL DEVIATION",
+          stockShortName: "Today",
+          price: {
+            current: 2.32,
+            open: 2.23,
+            low: 2.215,
+            high: 2.325,
+            cap: 93765011,
+            ratio: 20.1,
+            dividend: 1.67,
+          },
+          chartData: {
+            labels: data.map((sampling) => sampling.checkpoint),
+            data:  data.map((sampling) => sampling.rsL_DEVIATION),
+          },
+        }};
+        setData(info);
+      })
+      .catch((err) => {
+        console.log(err);
+      });}
+ 
+    };
+
+       
+
+
+  useEffect(() => {
+    fetch(variables.API_URL+'getAllAggBetweenDates', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+         "date1": new Date(''+date[0]).toISOString() ,
+  "date2": new Date(''+date[1]).toISOString()
+        }
+          
+          )
+          
+      })
+      .then((res) => res.json())
+      .then((data) => {
+ 
+        const info = {ob:{
+          stockFullName: "RSL DEVIATION",
+          stockShortName: "Today",
+          price: {
+            current: 2.32,
+            open: 2.23,
+            low: 2.215,
+            high: 2.325,
+            cap: 93765011,
+            ratio: 20.1,
+            dividend: 1.67,
+          },
+          chartData: {
+            labels: data.map((sampling) => sampling.checkpoint),
+            data:  data.map((sampling) => sampling.rsL_DEVIATION),
+          },
+        }};
+        setData(info);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [selectRange, setSelectRange] = useState(true);
+
+ const onChange = (dates) => {
+    console.log('onChange triggered', dates);
+
+    if (Array.isArray(dates)) {
+      const [start, end] = dates;
+      setStartDate(start);
+      setEndDate(end);
+
+      setSelectRange(false);
+      get( start,end);
+    } else {
+      setEndDate(dates);
+      setSelectRange(true);
+
+    }
+  };
+  /****************************************** */
+  return (
+
+    <div>
+    <div className="bg-image"></div>
+    <div className="bg-text">
+      <h2 className="text-2xl py-3">In Insight</h2>
+      <h1 className="text-4xl py-3 postTitle">Harness the power of change</h1>
+
+      <div
+        style={{ display: display ? "flex" :"flex" }}
+        className="flex justify-center align-middle my-4 p-2"
+      >
+          <form className="min-w-screen   flex items-center justify-center p20-5 py-1" >
+    <div className="min-w-screen    flex items-center justify-center p20-5 py-1">
+  
+<div className='app'>
+      <h1 className='text-center'> </h1>
+      <div className='calendar-container'>
+        <Calendar
+                onChange={onChange}
+                value={date}
+           defaultValue={date} 
+            endDate={endDate}
+                inline={true}
+                 selected={new Date()}
+                selectRange={selectRange}
+                showWeekNumbers={true}
+                startDate={new Date()}
+           
+        />
+      </div>
+      {endDate!= null ? (
+        <p className='text-center'>
+          <span className='bold'>Start:</span>{' '}
+          {startDate.toDateString()}
+          &nbsp;|&nbsp;
+          <span className='bold'>End:</span> {endDate.toDateString()}
+        </p>
+      ) : (
+        <p className='text-center'>
+          <span className='bold'>Default selected date:</span>{' '}
+          {endDate.toDateString()}
+        </p>
+      )}
+      
+      
+      </div>
+
+ 
+      </div>
+      
+<div className="min-w-screen    flex items-center justify-center px-5 py-5">
+
+<StockChart info={data.ob} />
+
+</div> 
+</form> 
+      </div>
+     
+      
+    </div>
+  </div>
+
+   
+  );
+}
+
+export default App;
